7ef84991ee29cf10418b77d6c8968431b1c62328
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-29 14:47:39 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,10 +5,11 @@ attr_accessor :name, :breed, :age
 @@all = []
 
   def initialize(name = nil, breed = nil, age = nil)
-        binding.pry
+
     @name = name
     @breed = breed
     @age = age
+            binding.pry
     @@all < self
 
   end
